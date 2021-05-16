f37b77d22c23bb6572cfc9bf457f26ca9dccf7df
Brian Cho <bischo91@gmail.com>
2021-05-16 00:03:44 -0400

    Zillow scrape - function to scrape year built and other home facts - ready to implement function, and also need to modulize

--- zillow_scrape_v1_test_version.py
+++ zillow_scrape_v1_test_version.py
@@ -105,152 +105,170 @@ def price2data(price):
         return 0, est
 
 def extract_and_store_data():
-    saved_link = []
-    saved_price= []
-    saved_price_change = []
-    # today_date = str(datetime.date.today()).replace('-','')
-    # i = 0
-    # con = sqlite3.connect("D:/Database/real_estate_database.db")
-    # cursor = con.cursor()
-    # while not cursor.fetchone():
-    #     i -= 1
-    #
-    #     prev_date = str(datetime.date.today() + datetime.timedelta(days = i)).replace('-','')
-    #     # prev_date = str(datetime.date(2021,2,20)).replace('-','')
-    #     table_name = "real_estate_" + prev_date
-    #     cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='" + table_name + "'")
-    #
-    # cursor.execute("select * from " + table_name)
-    # rows = cursor.fetchall()
-    # con_new = sqlite3.connect("D:/Database/real_estate_database.db")
-    # cursor_new = con_new.cursor()
-    # table_new = "real_estate_" + today_date
-    # cursor_new.execute("drop table if exists " + table_new)
-    # cursor_new.execute("create table " + table_new + "(price, bds, ba, sqft, acres, est, property, type, year, heating, cooling, parking, hoa, link, updated_on, last_price)")
-    # # cursor_new.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='" + table_new + "'")
-    # sql = 'insert into ' + table_name + ' values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
-    # sql_new = 'insert into ' + table_new + ' values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
-    #
-    # for row in rows:
-    #     saved_price.append(row[0])
-    #     saved_link.append(row[13])
 
     url ='https://www.zillow.com/homes/for_sale/gainesville-fl/'
+    url = 'https://www.zillow.com/homedetails/3330-NW-31st-Ave-Gainesville-FL-32605/42709451_zpid/'
     soup = url_opener(url)
-    if soup != None:
-        pages = []
-        current_pg = 'invalid'
-        next_pg = ''
-        while next_pg != current_pg:
-            current_pg = next_pg
-            if current_pg == '':
-                print('Page 1')
-            else:
-                print('Page ' + str(''.join(filter(lambda i: i.isdigit(), current_pg))))
-            url_pg = url + current_pg
-            pages.append(url_pg)
-            soup = url_opener(url_pg)
-            for element in soup.find_all(class_='list-card'):
-                house_url = element.find("a", class_='list-card-link')
-                house_url = house_url['href']
-                price = element.select_one('.list-card-price').text
-                price, est = price2data(price)
-                new_price = price
-                details = element.select_one('.list-card-details').text
-                if house_url in saved_link:
-                    j = saved_link.index(house_url)
-                    last_price_change = ''
-                    if int(new_price) == int(saved_price[j]):
-                        prev_price = rows[j][15]
-                        last_price_change = rows[j][14]
-                    else:
-                        # j=j+1
-                        print("price updated for row id " + str(j+1))
-                        prev_price = saved_price[j]
-                        last_price_change = today_date
-                    bds = rows[j][1]
-                    ba = rows[j][2]
-                    sqft = rows[j][3]
-                    acres = rows[j][4]
-                    est = rows[j][5]
-                    property = rows[j][6]
-                    type = rows[j][7]
-                    year = rows[j][8]
-                    heating = rows[j][9]
-                    cooling = rows[j][10]
-                    parking = rows[j][11]
-                    hoa = rows[j][12]
-                    # cursor_new.execute(sql_new, (new_price, bds, ba, sqft, acres, est, property, type, year, heating, cooling, parking, hoa, house_url, date_today, ''))
-                else:
-                    bds, ba, sqft, acres, property = detailstr2data(details)
-                    if est == True:
-                        est = 'Estimate'
-                    else:
-                        est = ''
-                    time.sleep(random.random()*10+random.random()*(random.random()*10))
-                    soup_house = url_opener(house_url)
-                    labellist =[]
-                    valuelist =[]
-                    factlabel_span = soup_house.find_all("span", {"class":"ds-home-fact-label"})
-                    factvalue_span = soup_house.find_all("span", {"class":"ds-home-fact-value"})
-                    # ----------------------------------------------------------------------------------------------------------------------
-                    test_ds_home_fact_list = soup_house.find_all("span", {"class":"ds-home-fact-list"})
-                    # ----------------------------------------------------------------------------------------------------------------------
-                    type = ''
-                    year = 0
-                    heating = ''
-                    cooling = ''
-                    parking = 0
-                    hoa = 0
-                    prev_price = ''
-                    # last_price_change = today_date
-                    for span in factlabel_span:
-                        label = span.text
-                        labellist.append(label)
-                    for span in factvalue_span:
-                        value = span.text
-                        valuelist.append(value)
-                    for label_text in labellist:
-                        i = labellist.index(label_text)
-                        if 'type' in label_text.lower():
-                            try:
-                                type = valuelist[i].lower()
-                            except ValueError:
-                                type = ''
-                        elif 'year built' in label_text.lower():
-                            try:
-                                year = int(valuelist[i])
-                            except ValueError:
-                                year = ''
-                        elif 'heating' in label_text.lower():
-                            try:
-                                heating = valuelist[i].lower()
-                            except ValueError:
-                                heating = ''
-                        elif 'cooling' in label_text.lower():
-                            try:
-                                cooling = valuelist[i].lower()
-                            except ValueError:
-                                cooling = ''
-                        elif 'parking' in label_text.lower():
-                            parking= valuelist[i]
-                            try:
-                                parking = int(''.join(j for j in parking if j.isdigit()))
-                            except ValueError:
-                                parking = valuelist[i]
-                        elif 'hoa' in label_text.lower():
-                            hoa = valuelist[i]
-                            try:
-                                hoa = int(''.join(j for j in hoa if j.isdigit()))
-                            except ValueError:
-                                hoa = valuelist[i]
-                #     cursor.execute(sql, (new_price, bds, ba, sqft, acres, est, property, type, year, heating, cooling, parking, hoa, house_url, last_price_change, prev_price))
-                # con.commit()
-                # cursor_new.execute(sql_new, (new_price, bds, ba, sqft, acres, est, property, type, year, heating, cooling, parking, hoa, house_url, last_price_change, prev_price))
-                # con_new.commit()
-            next_pg = soup.find("a", attrs={"title": "Next page"})['href']
+    house_details = soup.find("ul", class_='ds-home-fact-list')
+    # house_details = house_details.find_all('li',{"class": "ds-home-fact-list"})
+    house_details = house_details.find_all('li')
+    
+    type, year_built, heating, cooling, parking, hoa = '','','','','',''
+    for i in house_details:
+        element = i.text
+        element_list = element.split(':')
+        # element_list[0]
+        if element_list[0].lower() == 'type':
+            type = element_list[1]
+        elif element_list[0].lower() == 'year built':
+            year_built = int(element_list[1])
+        elif element_list[0].lower() == 'heating':
+            heating = element_list[1]
+        elif element_list[0].lower() == 'cooling':
+            cooling = element_list[1]
+        elif element_list[0].lower() == 'parking':
+            parking = element_list[1]
+        elif element_list[0].lower() == 'hoa':
+            hoa = element_list[1].replace('$','').replace(',','')
+    print(type)
+    print(year_built)
+    print(heating)
+    print(cooling)
+    print(parking)
+    print(hoa)
+    
+    # house_details = house_details.text
+    # house_details = house_details.lower()
+    # house_details = house_details.split(':')
+    # house_details = house_details.split('year built')
+    # house_details = house_details.split('heating')
+    # house_details = house_details.split('heating')
+    # house_details = house_details.split('cooling')
+    # house_details = house_details.split('parking')
+    
+    
+    # print(house_details)
+    
+    
 
-        print(pages)
+    # if soup != None:
+    #     pages = []
+    #     current_pg = 'invalid'
+    #     next_pg = ''
+    #     while next_pg != current_pg:
+    #         current_pg = next_pg
+    #         if current_pg == '':
+    #             print('Page 1')
+    #         else:
+    #             print('Page ' + str(''.join(filter(lambda i: i.isdigit(), current_pg))))
+    #         url_pg = url + current_pg
+    #         pages.append(url_pg)
+    #         soup = url_opener(url_pg)
+    #         for element in soup.find_all(class_='list-card'):
+    #             print(element)
+    #             house_url = element.find("a", class_='list-card-link')
+    #             house_url = house_url['href']
+    #             price = element.select_one('.list-card-price').text
+    #             price, est = price2data(price)
+    #             new_price = price
+    #             details = element.select_one('.list-card-details').text
+    #             if house_url in saved_link:
+    #                 j = saved_link.index(house_url)
+    #                 last_price_change = ''
+    #                 if int(new_price) == int(saved_price[j]):
+    #                     prev_price = rows[j][15]
+    #                     last_price_change = rows[j][14]
+    #                 else:
+    #                     # j=j+1
+    #                     print("price updated for row id " + str(j+1))
+    #                     prev_price = saved_price[j]
+    #                     last_price_change = today_date
+    #                 bds = rows[j][1]
+    #                 ba = rows[j][2]
+    #                 sqft = rows[j][3]
+    #                 acres = rows[j][4]
+    #                 est = rows[j][5]
+    #                 property = rows[j][6]
+    #                 type = rows[j][7]
+    #                 year = rows[j][8]
+    #                 heating = rows[j][9]
+    #                 cooling = rows[j][10]
+    #                 parking = rows[j][11]
+    #                 hoa = rows[j][12]
+    #                 # cursor_new.execute(sql_new, (new_price, bds, ba, sqft, acres, est, property, type, year, heating, cooling, parking, hoa, house_url, date_today, ''))
+    #             else:
+    #                 bds, ba, sqft, acres, property = detailstr2data(details)
+    #                 if est == True:
+    #                     est = 'Estimate'
+    #                 else:
+    #                     est = ''
+    #                 time.sleep(random.random()*10+random.random()*(random.random()*10))
+    #                 soup_house = url_opener(house_url)
+    #                 labellist =[]
+    #                 valuelist =[]
+    #                 # factlabel_span = soup_house.find_all("span", {"class":"ds-home-fact-label"})
+    #                 # factvalue_span = soup_house.find_all("span", {"class":"ds-home-fact-value"})
+    #                 house_details = soup_house.find("ul", class_='ds-home-fact-list')
+    #                 print(house_details)
+    # 
+    # 
+    #                 # ----------------------------------------------------------------------------------------------------------------------
+    #                 # test_ds_home_fact_list = soup_house.find_all("span", {"class":"ds-home-fact-list"})
+    #                 # ----------------------------------------------------------------------------------------------------------------------
+    #                 type = ''
+    #                 year = 0
+    #                 heating = ''
+    #                 cooling = ''
+    #                 parking = 0
+    #                 hoa = 0
+    #                 prev_price = ''
+    #                 # last_price_change = today_date
+    #                 for span in factlabel_span:
+    #                     label = span.text
+    #                     labellist.append(label)
+    #                 for span in factvalue_span:
+    #                     value = span.text
+    #                     valuelist.append(value)
+    #                 for label_text in labellist:
+    #                     i = labellist.index(label_text)
+    #                     if 'type' in label_text.lower():
+    #                         try:
+    #                             type = valuelist[i].lower()
+    #                         except ValueError:
+    #                             type = ''
+    #                     elif 'year built' in label_text.lower():
+    #                         try:
+    #                             year = int(valuelist[i])
+    #                         except ValueError:
+    #                             year = ''
+    #                     elif 'heating' in label_text.lower():
+    #                         try:
+    #                             heating = valuelist[i].lower()
+    #                         except ValueError:
+    #                             heating = ''
+    #                     elif 'cooling' in label_text.lower():
+    #                         try:
+    #                             cooling = valuelist[i].lower()
+    #                         except ValueError:
+    #                             cooling = ''
+    #                     elif 'parking' in label_text.lower():
+    #                         parking= valuelist[i]
+    #                         try:
+    #                             parking = int(''.join(j for j in parking if j.isdigit()))
+    #                         except ValueError:
+    #                             parking = valuelist[i]
+    #                     elif 'hoa' in label_text.lower():
+    #                         hoa = valuelist[i]
+    #                         try:
+    #                             hoa = int(''.join(j for j in hoa if j.isdigit()))
+    #                         except ValueError:
+    #                             hoa = valuelist[i]
+    #             #     cursor.execute(sql, (new_price, bds, ba, sqft, acres, est, property, type, year, heating, cooling, parking, hoa, house_url, last_price_change, prev_price))
+    #             # con.commit()
+    #             # cursor_new.execute(sql_new, (new_price, bds, ba, sqft, acres, est, property, type, year, heating, cooling, parking, hoa, house_url, last_price_change, prev_price))
+    #             # con_new.commit()
+    #         next_pg = soup.find("a", attrs={"title": "Next page"})['href']
+
 # url ='https://www.zillow.com/homes/gainesville_rb/'
 
 
